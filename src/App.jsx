835941de58503adac5dba3d02c0a642aeee5b450
dandanii22 <dandanii0816@gmail.com>
2024-07-12 04:18:36 +0900

    추가

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Header />
+        <DayList />
         <Routes>
-          <Route path="/" element={<DayList />} />
           <Route path="/day/:day" element={<Day />} />
           <Route path="/create_word" element={<CreateWord />} />
           <Route path="/create_day" element={<CreateDay />} />
